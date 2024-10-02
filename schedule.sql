ddfaba946adbe320ab5e0fd7f3b0b2fefeb076be
yuseonjun <seonjun0906@gmail.com>
2024-10-02 19:28:35 +0900

    [Docs] Level4 ERD 수정

--- schedule.sql
+++ schedule.sql
@@ -2,11 +2,21 @@ DROP TABLE IF EXISTS SCHEDULE;
 
 CREATE TABLE SCHEDULE
 (
-    id      BIGINT      NOT NULL AUTO_INCREMENT,
-    task    VARCHAR(20) NOT NULL,
-    name    VARCHAR(20) NOT NULL,
-    password VARCHAR(20) NOT NULL,
+    id         BIGINT      NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    task       VARCHAR(20) NOT NULL,
+    name       VARCHAR(20) NOT NULL,
+    password   VARCHAR(20) NOT NULL,
+    writer_id  BIGINT,
     created_at timestamp,
     updated_at timestamp,
-    PRIMARY KEY (id)
+    FOREIGN KEY (writer_id) REFERENCES WRITER (id)
+);
+
+CREATE TABLE WRITER
+(
+    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name       VARCHAR(255),
+    email      VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
